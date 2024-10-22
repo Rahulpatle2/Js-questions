1b3a208354effcd62e6b7b2cc41f1c605c7f0694
Rahul Patle <115938350+Rahulpatle2@users.noreply.github.com>
2024-10-22 22:46:00 +0530

    file added

---
+++ practice-JS/script.js
@@ -0,0 +1,60 @@
+//Given an integer input, The objective is to write a code to Check if a Number is Positive or Negative
+
+// let num = +prompt('Enter your number');
+
+// if(num > 0){
+//     console.log("the number is Positive");
+// }else if(num < 0){
+//     console.log("the number is Negative");
+// }else{
+//     console.log("zero");
+// }
+
+
+//Program to Check Whether a Number is Even or Odd
+
+// let num = +prompt("Enter any Number");
+
+// if(isNaN(num)){
+//     console.log("Please Enter a valid value");
+// }else if(num % 2 === 0){
+//     console.log("The Number is Even");
+// }else{
+//     console.log("The Number is Odd");
+// }
+
+// whenever we tries to convert a charter to a num javascript consider 
+// its as a NaN and it has a odd binary value therefore JS considerd it as odd num 
+
+
+//Program to Find the Sum of First N Natural Numbers
+// let sum = 0;
+// for(let i = 1; i <= 15; i++){
+   
+//    sum += i;
+  
+// }
+
+// console.log(sum);
+
+//Note: don't reassign a value inside a loop because it will cause a problem
+
+// Program to find the Sum of numbers in a given range
+
+// let num1 = 4;
+// let num2 = 8;
+// let sum = 0;
+
+// for(i = num1; i <= num2; i++){
+//     sum = sum + i;
+// }
+
+// //0+4 =4
+// //4+5 =9
+// //9+6 =15
+// //15+7 =22
+// //22+8 =30
+// //30
+
+// console.log(`the some of the numbers is ${sum}`)
+
